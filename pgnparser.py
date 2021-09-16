b8ca2fa1c8054d58fbad1386c33f0cc008373eef
arry-lee <arry_lee@qq.com>
2021-09-16 22:30:54 +0800

    add ChessBoard modify board2pic

--- pgnparser.py
+++ pgnparser.py
@@ -197,9 +197,232 @@ def flip_board(board):
 	return nb
 
 
+def draw_board(size=(1920,1080),bg_color=(0,0,0)):
+	"""绘制空棋盘"""
+	new_screen = Image.new('RGB',size,bg_color)
+	draw = ImageDraw.Draw(new_screen)
+
+	# size = (1920,1080)
+	w,h = size
+	ratio = h/1080
+
+
+	gz_width = int(105*ratio)
+	wmid = w//2
+	hmid = h//2
+	river_width = int(108*ratio)          # 河界宽
+	river_up = hmid - river_width//2
+	river_down = hmid + river_width//2
+
+
+	hlines = [river_up-i*gz_width for i in range(5)]+[river_down + i*gz_width for i in range(5)]
+	vlines = [wmid-i*gz_width for i in range(1,5)]+[wmid+i*gz_width for i in range(5)]
+	hlines.sort()
+	vlines.sort()
+
+
+	# assert hlines == [66, 171, 276, 381, 486, 594, 699, 804, 909, 1014]
+	# assert vlines == [540, 645, 750, 855, 960, 1065, 1170, 1275, 1380]
+
+
+	# 
+	# y_ratio = 720/1080
+
+	# hlines = [int(x*x_ratio) for x in hlines]
+	# vlines = [int(y*y_ratio) for y in vlines]
+
+	# new_screen = Image.new('RGB',(1280,720),bg_color)
+	# draw = ImageDraw.Draw(new_screen)
+
+	# print(hlines)
+	# print(vlines)
+
+	# 画网格
+	ww = int(3*ratio) 
+	print(ww)
+	for i in hlines:
+		draw.line((vlines[0],i)+(vlines[-1],i),fill='white',width=ww)
+
+	for i in vlines:
+		draw.line((i,hlines[0])+(i,hlines[4]),fill='white',width=ww)
+		draw.line((i,hlines[5])+(i,hlines[-1]),fill='white',width=ww)
+
+	draw.line((vlines[0],hlines[4])+(vlines[0],hlines[5]),fill='white',width=ww)
+	draw.line((vlines[-1],hlines[4])+(vlines[-1],hlines[5]),fill='white',width=ww)
+
+
+	# 画雪花
+	dd = int(8*ratio)
+	ll = 2*dd
+
+	def snow(x,y,d=8,l=16,width=1):
+		# 画棋盘雪花
+		draw.line((x+d,y+d)+(x+d,y+d+l),width=width)
+		draw.line((x+d,y+d)+(x+d+l,y+d),width=width)
+
+		draw.line((x-d+1,y-d+1)+(x-d+1,y-d+1-l),width=width)
+		draw.line((x-d+1,y-d+1)+(x-d+1-l,y-d+1),width=width)
+
+		draw.line((x-d+1,y+d)+(x-d+1,y+d+l),width=width)
+		draw.line((x-d+1,y+d)+(x-d+1-l,y+d),width=width)
+
+		draw.line((x+d,y-d+1)+(x+d,y-d+1-l),width=width)
+		draw.line((x+d,y-d+1)+(x+d+l,y-d+1),width=width)
+
+	def lsnow(x,y,d=8,l=16,width=1):
+		# 左雪花
+		draw.line((x-d+1,y-d+1)+(x-d+1,y-d+1-l),width=width)
+		draw.line((x-d+1,y-d+1)+(x-d+1-l,y-d+1),width=width)
+		draw.line((x-d+1,y+d)+(x-d+1,y+d+l),width=width)
+		draw.line((x-d+1,y+d)+(x-d+1-l,y+d),width=width)
+
+	def rsnow(x,y,d=8,l=16,width=1):
+		# 右雪花
+		draw.line((x+d,y+d)+(x+d,y+d+l),width=width)
+		draw.line((x+d,y+d)+(x+d+l,y+d),width=width)
+		draw.line((x+d,y-d+1)+(x+d,y-d+1-l),width=width)
+		draw.line((x+d,y-d+1)+(x+d+l,y-d+1),width=width)
+
+	rsnow(vlines[0],hlines[3],d=dd,l=ll)
+	snow(vlines[2],hlines[3],d=dd,l=ll)
+	snow(vlines[4],hlines[3],d=dd,l=ll)
+	snow(vlines[6],hlines[3],d=dd,l=ll)
+	lsnow(vlines[8],hlines[3],d=dd,l=ll)
+	snow(vlines[1],hlines[2],d=dd,l=ll)
+	snow(vlines[7],hlines[2],d=dd,l=ll)
+	rsnow(vlines[0],hlines[-4],d=dd,l=ll)
+	snow(vlines[2],hlines[-4],d=dd,l=ll)
+	snow(vlines[4],hlines[-4],d=dd,l=ll)
+	snow(vlines[6],hlines[-4],d=dd,l=ll)
+	lsnow(vlines[8],hlines[-4],d=dd,l=ll)
+	snow(vlines[1],hlines[-3],d=dd,l=ll)
+	snow(vlines[7],hlines[-3],d=dd,l=ll)
+
+	# 画九宫
+	draw.line((vlines[3],hlines[0])+(vlines[5],hlines[2]),fill='white',width=2)
+	draw.line((vlines[3],hlines[2])+(vlines[5],hlines[0]),fill='white',width=2)
+
+	draw.line((vlines[3],hlines[-1])+(vlines[5],hlines[-3]),fill='white',width=2)
+	draw.line((vlines[3],hlines[-3])+(vlines[5],hlines[-1]),fill='white',width=2)
+
+	# 画楚河汉界
+	fontsize = int(70*ratio)
+	font = ImageFont.truetype("static/font/李旭科书法.ttf", fontsize,encoding='gb')
+	draw.text((wmid,hmid), '楚河          汉界' , anchor='mm',font=font)
+
+	# 画外框
+	dx = int(15*ratio)
+	draw.rectangle((vlines[0]-dx,hlines[0]-dx)+(vlines[8]+dx,hlines[-1]+dx),width=ww,outline='white')
+
+	odx = int(60*ratio)
+	draw.rectangle((vlines[0]-odx,hlines[0]-odx)+(vlines[8]+odx,hlines[-1]+odx),width=1,outline='white')
+
+	return new_screen
+
+
+class ChessBoard(object):
+
+	PIECEMAP = {'k':'将','a':'士','b':'象','n':'马','r':'车','p':'卒','c':'炮',
+				 'K':'帅','A':'仕','B':'相','N':'馬','R':'車','P':'兵','C':'砲',}
+	PLACEHOLDER = '〇'
+
+	def __init__(self,board):
+		if isinstance(board, str):
+			if len(board) == 112:
+				self.board = ChessBoard.fromstring(board)
+			else:
+				self.board = ChessBoard.fromfen(board)
+		else:
+			self.board = board
+		self.red = True
+
+	def flip(self):
+		new_board = []
+		for row in reversed(self.board):
+			new_board.append(list(reversed(row)))
+		self.board = new_board
+		self.red = not self.red
+
+	def tostring(self):
+		return '\n'.join(''.join(row) for row in self.board)
+
+	def __str__(self):
+		return self.tostring()
+
+
+	@staticmethod
+	def fromstring(text,placeholder='〇'):
+		"""将局面字符串转化为二维棋盘数组"""
+		board = [[placeholder for i in range(9)] for j in range(10)]
+
+		for i, line in enumerate(text.split()):
+			for j, piece in enumerate(line.strip()):
+				board[i][j] = piece
+		return board
+
+	@staticmethod
+	def fromfen(fen):
+		text = ChessBoard.fen2txt(fen)
+		board = ChessBoard.fromstring(text)
+		return board
+
+
+	@staticmethod
+	def fen2txt(fen):
+		"""读取fen文件，返回局面字符串"""
+		fen = fen.split()[0]		
+		board = []
+		for l in fen.split('/'):
+			line = ''
+			for x in l:
+				if x.isdigit():
+					line += ChessBoard.PLACEHOLDER * int(x)
+				else:
+					line += PIECEMAP[x]
+			board.append(line)
+		return '\n'.join(board)
+
+
+	def topicture(self,bg_color=(0,0,0)):
+		try:
+			new_screen = Image.open('default_board.png')
+		except:
+			new_screen = draw_board(bg_color)
+			new_screen.save('default_board.png')
+		draw = ImageDraw.Draw(new_screen)
+
+		hlines = [64, 169, 274, 379, 484, 593, 698, 803, 908, 1013]
+		vlines = [15, 120, 225, 330, 435, 540, 645, 750, 855, 960, 1065, 1170, 1275, 1380, 1485, 1590, 1695, 1800, 1905]
+		board = [[(v,h) for v in vlines[5:5+9]] for h in hlines[0:10]]
+
+		# 绘制棋子
+		for i in range(9):
+			for j in range(10):
+				v,h = board[j][i]
+				p = self.board[j][i]
+				if p != ChessBoard.PLACEHOLDER:
+					chessman_folder='static/img/chess/'
+					chess = Image.open(chessman_folder + p + '.png')
+					w = 120
+					chess = chess.resize((w,w))
+					new_screen.paste(chess,(v-w//2,h-w//2)+(v+w//2,h+w//2),mask=chess)
+
+		# new_screen = new_screen.resize((1280,720))
+		# new_screen.save('board720.png')
+		return new_screen
+
+
+	def show(self):
+		self.topicture().show()
+
+
+
 def board2pic(b,players = None,bg_color=(118,37,47), 
 			  background=None, fr=None, to=None, choices=None,
 			  comment=None,rotate=False,flip=False,blind=False):
+	"""
+	将board数组转换为图片
+	"""
 	if flip is True:
 		b = flip_board(b)
 
@@ -223,8 +446,6 @@ def board2pic(b,players = None,bg_color=(118,37,47),
 	if not background:
 		# 参考线
 		# 大棋盘框
-		
-
 
 		for i in hlines:
 			draw.line((vlines[5],i)+(vlines[13],i),fill='white',width=3)
@@ -607,150 +828,6 @@ def ubb2gif(ubb,out_file):
 	return frame
 
 
-
-
-
-
-class Board(object):
-	def __init__(self,fen):
-		if len(fen)>60:
-			self.board = txt2board(fen)
-		else:
-			self.board = fen2board(fen)
-		self.direction = 1
-
-	def flip(self):
-		self.direction = - self.direction
-		new_board = []
-		for l in reversed(self.board):
-			new_board.append(list(reversed(l)))
-		self.board = new_board
-
-	def to_string(self):
-		return '\n'.join(''.join(line) for line in board)
-
-	def __str__(self):
-		return self.to_string()
-
-
-	def to_image(self,bg_color=(118,37,47), size=(1920,1080)):
-		board_image = Image.new('RGB',size,bg_color)
-		draw = ImageDraw.Draw(board_image)
-		hlines =  sorted(list(range(484,0,-105))+list(range(593,1080,105)))
-		vlines = sorted(list(set(list(range(960,0,-105))+list(range(960,1920,105)))))
-
-		for i in hlines:
-			draw.line((vlines[5],i)+(vlines[13],i),fill='white',width=3)
-
-		for i in vlines[5:14]:
-			draw.line((i,hlines[0])+(i,hlines[4]),fill='white',width=3)
-
-		for i in vlines[5:14]:
-			draw.line((i,hlines[5])+(i,hlines[-1]),fill='white',width=3)
-
-		def snow(x,y,d=8,l=16):
-			draw.line((x+d,y+d)+(x+d,y+d+l),width=1)
-			draw.line((x+d,y+d)+(x+d+l,y+d),width=1)
-
-			draw.line((x-d+1,y-d+1)+(x-d+1,y-d+1-l),width=1)
-			draw.line((x-d+1,y-d+1)+(x-d+1-l,y-d+1),width=1)
-
-			draw.line((x-d+1,y+d)+(x-d+1,y+d+l),width=1)
-			draw.line((x-d+1,y+d)+(x-d+1-l,y+d),width=1)
-
-			draw.line((x+d,y-d+1)+(x+d,y-d+1-l),width=1)
-			draw.line((x+d,y-d+1)+(x+d+l,y-d+1),width=1)
-
-		def lsnow(x,y,d=8,l=16):
-			draw.line((x-d+1,y-d+1)+(x-d+1,y-d+1-l),width=1)
-			draw.line((x-d+1,y-d+1)+(x-d+1-l,y-d+1),width=1)
-
-			draw.line((x-d+1,y+d)+(x-d+1,y+d+l),width=1)
-			draw.line((x-d+1,y+d)+(x-d+1-l,y+d),width=1)
-
-		def rsnow(x,y,d=8,l=16):
-			draw.line((x+d,y+d)+(x+d,y+d+l),width=1)
-			draw.line((x+d,y+d)+(x+d+l,y+d),width=1)
-
-			draw.line((x+d,y-d+1)+(x+d,y-d+1-l),width=1)
-			draw.line((x+d,y-d+1)+(x+d+l,y-d+1),width=1)
-
-		rsnow(vlines[5],hlines[3])
-
-		snow(vlines[7],hlines[3])
-		snow(vlines[9],hlines[3])
-		snow(vlines[11],hlines[3])
-		lsnow(vlines[13],hlines[3])
-		snow(vlines[6],hlines[2])
-		snow(vlines[12],hlines[2])
-
-		rsnow(vlines[5],hlines[-4])
-		snow(vlines[7],hlines[-4])
-		snow(vlines[9],hlines[-4])
-		snow(vlines[11],hlines[-4])
-		lsnow(vlines[13],hlines[-4])
-		snow(vlines[6],hlines[-3])
-		snow(vlines[12],hlines[-3])
-
-		draw.line((vlines[5],hlines[4])+(vlines[5],hlines[5]),fill='white',width=3)
-		draw.line((vlines[13],hlines[4])+(vlines[13],hlines[5]),fill='white',width=3)
-
-		draw.line((vlines[8],hlines[0])+(vlines[10],hlines[2]),fill='white',width=2)
-		draw.line((vlines[8],hlines[2])+(vlines[10],hlines[0]),fill='white',width=2)
-
-		draw.line((vlines[8],hlines[-1])+(vlines[10],hlines[-3]),fill='white',width=2)
-		draw.line((vlines[8],hlines[-3])+(vlines[10],hlines[-1]),fill='white',width=2)
-
-		fontsize = 70
-		font = ImageFont.truetype("static/font/李旭科书法.ttf", fontsize,encoding='gb')
-		draw.text((vlines[6],hlines[4]+18), '楚河〇〇〇〇〇汉界' , font=font)
-
-		
-		draw.rectangle((vlines[5]-15,hlines[0]-15)+(vlines[13]+15,hlines[-1]+15),width=4,outline='white')
-		draw.rectangle((vlines[5]-60,hlines[0]-60)+(vlines[13]+60,hlines[-1]+60),width=1)
-			
-		board = [[(v,h) for v in vlines[5:5+9]] for h in hlines[0:10]]
-
-		for i in range(9):
-			for j in range(10):
-				v,h = board[j][i]
-				p = self.board[j][i]
-
-				if p in '車馬相仕帅炮兵':
-					color = '#82111f'
-					bg_color = 'white'
-				else:
-					color = 'white'
-					bg_color = 'black'
-
-				if p != '〇':
-					chessman_folder='static/img/chess/'
-					chess = Image.open(chessman_folder + p + '.png')
-					w = 120
-					chess = chess.resize((w,w))
-					board_image.paste(chess,(v-w//2,h-w//2)+(v+w//2,h+w//2),mask=chess)
-
-		# top_l = Image.open('static/img/download/DH_bg_top_l.png').convert('RGBA')
-		# board_image.paste(top_l,(0,0),mask=top_l)
-		# top_r = Image.open('static/img/download/DH_bg_top_r.png').convert('RGBA')
-		# board_image.paste(top_r,(1920-top_r.width,0),mask=top_r)
-
-		# top_lu = Image.open('static/img/download/DH_bg_top_lu.png').convert('RGBA')
-		# board_image.paste(top_lu,(1920-top_lu.width-200,0),mask=top_lu)
-
-		# down_l = Image.open('static/img/download/DH_bg_down_l.png').convert('RGBA')
-		# board_image.paste(down_l,(0,1080-down_l.height),mask=down_l)
-
-		# down_r = Image.open('static/img/download/DH_bg_down_r.png').convert('RGBA')
-		# board_image.paste(down_r,(1920-down_r.width,1080-down_r.height),mask=down_r)
-
-		return board_image
-
-	def show(self):
-		self.board_image = self.to_image()
-		self.board_image.show()
-
-
 if __name__ == '__main__':
 	s = \
 	"""
@@ -766,15 +843,15 @@ if __name__ == '__main__':
 	車馬相仕帅仕相馬車
 	"""
 
-	# b = Board(s)
+	x = draw_board(size=(1280,720))
+	x.show()
+	# b = ChessBoard(s)
+	# print(b)
 	# b.flip()
+	# print(b)
 	# b.show()
-	# ubb2gif('md.ubb','md.mp4')
-	# b = '7999999949999999699977999985523140994299509999999999991738475899'
-	# print(binit2board(b))
-	# board2pic(binit2board(b)).show()
-
-	board2pic(txt2board(s),background=False,players=None).show()
+	# print(len(s))
+	# board2pic(txt2board(s),background=False,players=None).show()
 	# from bili import comment
 
 	# import time
